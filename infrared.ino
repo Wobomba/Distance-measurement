a3af1433ecf559c8d62e138dc931727f01423f68
elywin <lukeeraelywin@gmail.com>
2022-07-04 16:19:52 +0300

    measure distance

--- infrared.ino
+++ infrared.ino
@@ -1,26 +1,31 @@
 int led = 12;
-int sensor= 8;
+int sensor= 0;
+int val = 0;                 // variable to store the values from sensor(initially zero)
 
 void setup()
 {
-  pinMode(led,OUTPUT);
-  pinMode(sensor,INPUT);
+//  pinMode(led,OUTPUT);
+//  pinMode(sensor,INPUT);
   Serial.begin(9600);
 }
 void loop()
 {
-  if (digitalRead(sensor)== LOW)
-  {
-    digitalWrite(led,HIGH);
-    
-    delay(10);
-  }
-  else 
-  {
-    
-    digitalWrite(led,LOW);
-    delay(10);
-    
-  }
+
+  val = analogRead(sensor);       // reads the value of the sharp sensor
+  Serial.println(val);     
+   delay(100); 
+//  if (digitalRead(sensor)== LOW)
+//  {
+//    digitalWrite(led,HIGH);
+//    
+//    delay(10);
+//  }
+//  else 
+//  {
+//    
+//    digitalWrite(led,LOW);
+//    delay(10);
+//    
+//  }
   
 }
